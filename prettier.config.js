f012d14abed0a9b278d73949d21e526b3d1313d7
joaozandona <joaoricardozandona@hotmail.com>
2022-03-09 22:01:03 -0300

    Iniciando o projeto ainda

---
+++ prettier.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  singleQuoque: false,
+};
---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+
+import { CategoriesRepository } from "../repositories/CategoriesRespository";
+
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
+
+categoriesRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+  categoriesRepository.create({ name, description });
+  return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all).send();
+});
+
+export { categoriesRoutes };
